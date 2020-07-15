e4a64b4990fa1e88d11e3517f0aa0e09c0d0875a
amaparicio22 <64286119+amaparicio22@users.noreply.github.com>
2020-07-15 11:45:52 -0500

    Update 7.15.2020.py

--- 7.15.2020.py
+++ 7.15.2020.py
@@ -5,6 +5,12 @@ Created on Wed Jul 15 10:59:58 2020
 @author: Amanda Aparicio
 """ 
 def make_additions(num_list):
+    """Takes a list of numbers and outputs a list of all possible addition combinations
+        of those numbers. 
+        input:
+            num_list: list of numbers (int or float, positive or negative)
+        output:
+            additions: list of numbers (int or float, positive or negative)"""
     additions = []
     n = len(num_list)
     i = 0
@@ -17,6 +23,14 @@ def make_additions(num_list):
     return additions
     
 def compare_additions(num_list, k):
+    """ Returns True if k is equal to the addition of two elements in num_list. Otherwise
+    returns False.
+    inputs:
+        num_list: list of numbers (int or float, positive or negative)
+        k: number (int or float, positive or negative)
+    output:
+        True or False (boolean)
+    """
     try:
         all_add_combos = make_additions(num_list)
         if (k in all_add_combos):
